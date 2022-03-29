3d55b4d2fcf3c0ce8b9b692c62a4d4163d1ed774
Khadim92 <imkhadim1@gmail.com>
2022-03-29 07:13:42 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Circle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float r = 2f;
+            Console.Write("Enter radius of circle : ");
+            r=float.Parse(Console.ReadLine());
+            circle c = new circle("Blue",r);
+            Console.WriteLine("Radius = "+c.radius);
+            float area, circumference,diameter;
+            area = c.Area();
+            circumference=c.circumference();
+            diameter = c.diameter();
+            Console.WriteLine("Diameter : "+diameter);
+            Console.WriteLine("Area : "+area);
+            Console.WriteLine("Circumference : "+circumference);
+        }
+    }
+}
---
+++ circle.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Circle
+{
+    internal class circle
+    {
+        public circle()
+        {
+            radius = 1.0f;
+            color="red";
+        }
+        public circle(int r)
+        {
+            radius = r;
+        }
+        public circle(string c, float r)
+        {
+            color = c;
+            radius = r;
+        }
+        public circle(circle c)
+        {
+            color = c.color;
+            radius = c.radius;
+        }
+        public float radius;
+        public string color;
+        public float Area()
+        {
+            float pi = 3.14f;
+            float area = pi * radius * radius;
+            return area;
+        }
+        public float circumference()
+        {
+            float pi = 3.14f;
+            float cir = 2*pi * radius ;
+            return cir;
+        }
+        public float diameter()
+        {
+            return 2 * radius;
+        }
+    }
+}
